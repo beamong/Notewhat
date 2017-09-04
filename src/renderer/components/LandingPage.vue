ab00b4a30663128f06135ddc53d6f8e2aebf4c1b
JeongHyeonJu <hyeon6921@gmail.com>
2017-09-04 22:32:59 +0900

    layout

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="holygrail">
-    <div class="holygrail-header">
-      <nav class="holygrail-nav">
+  <div class="container">
+    <div class="container-header">
+      <nav class="header-brand">
         <span class="brand">
           <span>NOTE</span>
           <span class="sub">WHAT</span>
         </span>
       </nav>
-      <nav class="holygrail-content">
+      <nav class="header-content">
         <div class="buttons">
           <i class="fa fa-plus"></i>
         </div>
@@ -18,8 +18,8 @@
         </div>
       </nav>
     </div>
-    <div class="holygrail-body">
-      <nav class="holygrail-nav">
+    <div class="container-body">
+      <nav class="list">
         <div :class="entry.isActive ? 'active' : ''" :key="entry.id" v-for="entry in entries">
           <h1>{{entry.title}}</h1>
           <div class="content">
@@ -27,22 +27,16 @@
           </div>
         </div>
       </nav>
-
-      <main class="holygrail-content">
-        <div class="holygrail-editor">
-          <div class="editor">
-            <codemirror :code="code" :options="editorOptions" @ready="onEditorReady" @change="onEditorCodeChange"></codemirror>
-          </div>
-          <div v-if="keyBuffer" class="keybuffer-placeholder">
-            Key buffer:
-            <span style="">{{keyBuffer}}</span>
-          </div>
-        </div>
-        <aside class="holygrail-preview">
-          <div class="markdown-body" v-html="transpiled"></div>
-        </aside>
-      </main>
-
+      <div class="note">
+        <codemirror :code="code" :options="editorOptions" @ready="onEditorReady" @change="onEditorCodeChange"></codemirror>
+        <!-- <div v-if="keyBuffer" class="keybuffer-placeholder">
+          Key buffer:
+          <span>{{keyBuffer}}</span>
+        </div> -->
+      </div>
+      <div class="markdown">
+        <div class="markdown-body" v-html="transpiled"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -132,16 +126,13 @@ export default {
 
 body {
   font-family: 'Source Sans Pro', sans-serif;
-}
-
-.editor {
   height: 100%;
 }
 
 .CodeMirror,
 .CodeMirror-scroll {
-  height: 100%;
   width: 100%; // max-width: 500px;
+  height: inherit;
   opacity: 0.9;
 }
 
@@ -161,113 +152,105 @@ body {
   font-size: 0.9em;
 }
 
-.holygrail {
+.container {
   display: flex;
-  min-height: 100vh;
   flex-direction: column;
-}
-
-.holygrail-header {
-  display: flex;
-  flex: 1 0 3em;
-  flex-direction: row;
-  background: #f7f7f7;
-  border-bottom: 1px solid #EFEFEF;
-
-  .holygrail-content {
-    flex: 1;
+  width: 100%;
+  height: 100vh;
+  .container-header {
     display: flex;
-    .buttons {
-      padding: 1.0em;
-      flex: 1,
+    flex: 0 0 3em;
+    .header-brand {
+      flex: 0 0 20em;
+      order: -1;
+      padding: 1.1em;
+      font-size: 0.80em;
+      .brand {
+        font-size: 1.3em;
+        font-weight: bold;
+        font-family: sans-serif;
+        color: #595959;
+        span {
+          vertical-align: middle;
+        }
+        >.sub {
+          color: #adadad;
+        }
+      }
     }
-    .search {
-      font-size: 1em;
-      padding: 0.7em;
+    .header-content {
       flex: 1;
-      text-align: right;
-      input[type=text] {
+      display: flex;
+      .buttons {
+        padding: 1.0em;
+        flex: 1;
+        display: flex;
+        align-items: center;
+      }
+      .search {
+        font-size: 1em;
+        padding: 0.7em;
+        flex: 1;
+        text-align: right;
+        input[type=text] {
+          width: 20em;
+          padding: 0.4em;
+          font-size: 0.8em;
+        }
+      }
+    }
+  }
+  .container-body {
+    display: flex;
+    flex: 1;
+    border-top: 1px solid #eee;
+    overflow: auto;
+    .list {
+      flex: 0 0 20em;
+      overflow: auto;
+      font-size: 0.80em;
+      >div {
         width: 20em;
-        padding: 0.4em;
-        font-size: 0.8em;
+        border-bottom: 1px solid #EFEFEF;
+        color: #626f73;
+        padding: 10px;
+        &.active {
+          background: #fce28d;
+        }
+        h1,
+        .content {
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
+        }
+        h1 {
+          font-size: 1em;
+          margin-bottom: 5px;
+          color: #525f63;
+          font-weight: bold;
+        }
       }
     }
-  }
-  .holygrail-nav {
-    flex: 0 0 20em;
-    order: -1;
-    padding: 1.1em;
-    font-size: 0.80em;
-    .brand {
-      font-size: 1.3em;
-      font-weight: bold;
-      font-family: sans-serif;
-      color: #595959;
-      span.sub {
-        color: #adadad;
-      }
-    }
-  }
-}
-
-.holygrail-body {
-  display: flex;
-  flex: 1;
-
-  .holygrail-content {
-    flex: 1;
-    display: flex;
-
-    .holygrail-editor {
+    .note {
       flex: 1;
-      position: relative;
-      border-left: 1px solid #EFEFEF;
-      .keybuffer-placeholder {
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        font-size: 13px
-      }
+      border-left: 1px solid #eee;
+      overflow: auto;
+      // .keybuffer-placeholder {
+      //   position: absolute;
+      //   right: 0;
+      //   bottom: 0;
+      //   font-size: 13px
+      // }
     }
-
-    .holygrail-preview {
+    .markdown {
       flex: 1;
       overflow: auto;
       padding: 10px;
       border-left: 1px solid #efefef;
     }
   }
-
-  .holygrail-nav {
-    /* 12em is width of the column */
-    order: -1;
-    flex: 0 0 20em;
-    overflow: auto; // background: #f1f6f8;
-    font-size: 0.80em;
-    >div {
-      width: 20em;
-      border-bottom: 1px solid #EFEFEF;
-      color: #626f73;
-      padding: 10px;
-      &.active {
-        background: #fce28d;
-      }
-      h1,
-      .content {
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-      }
-      h1 {
-        font-size: 1em;
-        margin-bottom: 5px;
-        color: #525f63;
-        font-weight: bold;
-      }
-      .content {}
-    }
-  }
 }
 
 @import './markdown.css';
 </style>
+
